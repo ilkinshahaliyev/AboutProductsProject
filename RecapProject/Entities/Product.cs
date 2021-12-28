905b3e78569a35a764c8ba7078c4cdf8f7221bce
Ilkin Shahaliyev <shahaliyev2021@gmail.com>
2021-12-28 17:51:35 +0400

    Project Updated!

--- RecapProject/Entities/Product.cs
+++ RecapProject/Entities/Product.cs
@@ -12,7 +12,7 @@ namespace RecapProject.Entities
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
         public decimal UnitPrice { get; set; }
-        public Int16 UnitsInStock { get; set; }
+        public short UnitsInStock { get; set; }
         public string QuantityPerUnit { get; set; }
     }
 }
